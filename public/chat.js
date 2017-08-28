0f91e10f4b1e8d603e1b1ce39c600fb42ccb7b30
rishabh-malik <rishabhmalik249@gmail.com>
2017-08-28 20:19:12 +0530

    emitting event

--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,16 @@
 //make connection
 var socket=io.connect('http://localhost:4000');
+
+// Query DOM
+var message=document.getElementById('message');
+var handle=document.getElementById('handle');
+var btn=document.getElementById('send');
+var output=document.getElementById('output');
+
+//emitting event
+btn.addEventListener('click',function(){
+    socket.emit('chat',{
+        message: message.value,
+        handle: handle.value
+    });
+});
